f50750028971335254352d9eede8162405ed1f9c
HARSH JAIN <jainharsh270@gmail.com>
2020-04-08 12:11:08 +0530

    Updates

---
+++ precache-manifest.249668a5ec3ebb9b561c1272bd381892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4f46a74e103c3906c5c801f0726c29",
+    "url": "/Pokemon-Card-Game/index.html"
+  },
+  {
+    "revision": "428303b5ac23cdef1607",
+    "url": "/Pokemon-Card-Game/static/css/main.a36f2013.chunk.css"
+  },
+  {
+    "revision": "ba1764e79b554a62e694",
+    "url": "/Pokemon-Card-Game/static/js/2.54d62e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon-Card-Game/static/js/2.54d62e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428303b5ac23cdef1607",
+    "url": "/Pokemon-Card-Game/static/js/main.70e0cd96.chunk.js"
+  },
+  {
+    "revision": "13e71e2ebb2ffbfbf53b",
+    "url": "/Pokemon-Card-Game/static/js/runtime-main.f760f188.js"
+  }
+]);
